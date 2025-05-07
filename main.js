310a9ee4f4871a203072e312dbdee38e491a2e3c
Robin <booksofcodeandpsychic@gmail.com>
2025-05-07 11:50:18 -0400

    Added drop-down menu in navbar && New Page Added falcon devlog page

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('STOK_devlog').addEventListener('click', function() {
+    window.location.href = this.value;
+});
+
+document.getElementById('FALCON_devlog').addEventListener('click', function() {
+    window.location.href = this.value;
+});
+
